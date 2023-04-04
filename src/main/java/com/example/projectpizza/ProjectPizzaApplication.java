2409de50ae69771d0f80a311a0594181ae15136a
romanthium <romanthium@gmail.com>
2023-04-04 18:25:11 +0200

    initial commit

---
+++ src/main/java/com/example/projectpizza/ProjectPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectpizza/ProjectPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
