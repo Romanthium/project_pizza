f60509e306457a862d05af44b1959eab9ad0102b
romanthium <romanthium@gmail.com>
2023-04-19 19:48:46 +0200

    added dishes repository in Cafe controller

--- src/main/java/com/example/projectpizza/controller/CafesController.java
+++ src/main/java/com/example/projectpizza/controller/CafesController.java
@@ -1,8 +1,8 @@
 package com.example.projectpizza.controller;
 
 import com.example.projectpizza.model.Cafe;
-import com.example.projectpizza.repository.CafeRepository;
 import com.example.projectpizza.service.CafeService;
+import com.example.projectpizza.service.DishService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/cafes")
 public class CafesController {
     private final CafeService cafeService;
+    private final DishService dishService;
 
     @Autowired
-    public CafesController(CafeService cafeService) {
+    public CafesController(CafeService cafeService, DishService dishService) {
         this.cafeService = cafeService;
+        this.dishService = dishService;
     }
 
     @GetMapping()
@@ -33,15 +35,19 @@ public class CafesController {
     }
 
     @GetMapping("/new")
-    public String newCafe(@ModelAttribute("cafe") Cafe cafe) {
+    public String newCafe(@ModelAttribute("cafe") Cafe cafe, Model model) {
+        model.addAttribute("dishes", dishService.findAll());
         return "cafes/new";
     }
 
     @PostMapping()
     public String create(@ModelAttribute("cafe") @Valid Cafe cafe,
-                         BindingResult bindingResult) {
-        if (bindingResult.hasErrors())
+                         BindingResult bindingResult, Model model) {
+        model.addAttribute("dishes", dishService.findAll());
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("dishes", dishService.findAll());
             return "cafes/new";
+        }
 
         cafeService.save(cafe);
         return "redirect:/cafes";
@@ -49,6 +55,7 @@ public class CafesController {
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("dishes", dishService.findAll());
         model.addAttribute("cafe", cafeService.findOne(id));
         return "cafes/edit";
     }
@@ -56,9 +63,12 @@ public class CafesController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("cafe") @Valid Cafe cafe,
                          BindingResult bindingResult,
-                         @PathVariable("id") int id) {
-        if (bindingResult.hasErrors())
+                         @PathVariable("id") int id, Model model) {
+        model.addAttribute("dishes", dishService.findAll());
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("dishes", dishService.findAll());
             return "cafes/edit";
+        }
 
         cafeService.update(id, cafe);
         return "redirect:/cafes";
