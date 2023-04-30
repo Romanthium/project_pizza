eedcbe683b224e5546990b31f7efd2b996d2e65f
romanthium <romanthium@gmail.com>
2023-04-30 22:53:20 +0200

    added userDetailsService() to ApplicationConfig

--- src/main/java/com/example/projectpizza/config/ApplicationConfig.java
+++ src/main/java/com/example/projectpizza/config/ApplicationConfig.java
@@ -1,9 +1,20 @@
 package com.example.projectpizza.config;
 
+import com.example.projectpizza.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
