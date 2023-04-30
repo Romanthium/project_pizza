df260f76b53c110b5e2485c54aea4ddd4fb35d24
romanthium <romanthium@gmail.com>
2023-04-30 23:45:41 +0200

    added authenticationProvider() to ApllicationConfig

--- src/main/java/com/example/projectpizza/config/ApplicationConfig.java
+++ src/main/java/com/example/projectpizza/config/ApplicationConfig.java
@@ -4,6 +4,8 @@ import com.example.projectpizza.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -16,5 +18,13 @@ public class ApplicationConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
