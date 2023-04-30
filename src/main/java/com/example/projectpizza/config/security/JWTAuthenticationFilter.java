67f98521a3d916527e7aa1308375eba5f16aeb52
romanthium <romanthium@gmail.com>
2023-04-30 16:08:35 +0200

    added doFilterInternal method to JWTAuthenticationFilter class in configuration

--- src/main/java/com/example/projectpizza/config/security/JWTAuthenticationFilter.java
+++ src/main/java/com/example/projectpizza/config/security/JWTAuthenticationFilter.java
@@ -1,4 +1,28 @@
 package com.example.projectpizza.config.security;
 
-public class JWTAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+    }
 }
