2fa26ed5caf3e3992a0ce2eb3763d4061b20c08f
romanthium <romanthium@gmail.com>
2023-04-30 22:47:23 +0200

    added ApplicationConfig

---
+++ src/main/java/com/example/projectpizza/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.example.projectpizza.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+}
