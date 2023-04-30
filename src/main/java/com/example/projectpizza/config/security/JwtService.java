53d442e259aa74fbe57c77f913f48b6540c1764d
romanthium <romanthium@gmail.com>
2023-04-30 18:15:20 +0200

    added generateToken() in JwtService

--- src/main/java/com/example/projectpizza/config/security/JwtService.java
+++ src/main/java/com/example/projectpizza/config/security/JwtService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -26,6 +27,10 @@ public class JwtService {
     public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(jwtToken);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
