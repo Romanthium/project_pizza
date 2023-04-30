cda8dd7a10326bc7195e4916204f0e422b9a75c8
romanthium <romanthium@gmail.com>
2023-04-30 16:58:51 +0200

    added extractAllClaims to JwtService

--- src/main/java/com/example/projectpizza/config/security/JwtService.java
+++ src/main/java/com/example/projectpizza/config/security/JwtService.java
@@ -1,10 +1,21 @@
 package com.example.projectpizza.config.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
     public String extractUserlogin(String jwtToken) {
         return null;
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(jwtToken)
+                .getBody();
     }
 }
