e7e0094fc951fd098b56d50a2225236e53af6088
romanthium <romanthium@gmail.com>
2023-04-30 17:43:48 +0200

    added key & getSignKey() to JwtService

--- src/main/java/com/example/projectpizza/config/security/JwtService.java
+++ src/main/java/com/example/projectpizza/config/security/JwtService.java
@@ -2,10 +2,16 @@ package com.example.projectpizza.config.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "25442A472D4B6150645267556B58703273357638792F423F4528482B4D625165"; //toDo: move to properties file
     public String extractUserlogin(String jwtToken) {
         return null;
     }
@@ -18,4 +24,11 @@ public class JwtService {
                 .parseClaimsJws(jwtToken)
                 .getBody();
     }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
 }
