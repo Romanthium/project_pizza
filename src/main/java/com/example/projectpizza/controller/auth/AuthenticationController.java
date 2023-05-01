b1f29c41522bee0353a59d8ea55f3a3fe461e44e
romanthium <romanthium@gmail.com>
2023-05-01 23:41:43 +0200

    updated AuthenticationController

--- src/main/java/com/example/projectpizza/controller/auth/AuthenticationController.java
+++ src/main/java/com/example/projectpizza/controller/auth/AuthenticationController.java
@@ -1,9 +1,12 @@
 package com.example.projectpizza.controller.auth;
 
 import com.example.projectpizza.model.User;
+import com.example.projectpizza.service.UserRoleService;
+import com.example.projectpizza.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,14 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final UserService userService;
+    private final UserRoleService userRoleService;
     @GetMapping("/registration")
-    public String registrationPage(@ModelAttribute("user") User user) {
+    public String registrationPage(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("roles", userRoleService.findAll());
         return "auth/registration";
     }
 
     @PostMapping("/registration")
     public String registration(@ModelAttribute("user") @Valid User user) {  //todo: make validator for unique name
-        return null;
+        userService.save(user);                                             //toDo: make separate controller for users
+        return "redirect:/";
     }
 
     @GetMapping("/login")
