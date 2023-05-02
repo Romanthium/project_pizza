9e24b97ba07d78529bd8e512729bf6be4976d09c
romanthium <romanthium@gmail.com>
2023-05-02 16:19:04 +0200

    changed SecurityConfig to without jwt

--- src/main/java/com/example/projectpizza/config/security/SecurityConfig.java
+++ src/main/java/com/example/projectpizza/config/security/SecurityConfig.java
@@ -1,38 +1,39 @@
 package com.example.projectpizza.config.security;
 
+import com.example.projectpizza.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
-    /* to work without jwt
-    private final UserDetailsService userDetailsService;
-     */
+    private final UserRepository userRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        /* to work without jwt
+
         AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService);
+        authenticationManagerBuilder.userDetailsService(userDetailsService());
         AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
-         */
+
         httpSecurity
-                .csrf()
-                .disable()
-//                .authenticationManager(authenticationManager) // to work without jwt
+//                .csrf()
+//                .disable()
+                .authenticationManager(authenticationManager)
                 .authorizeHttpRequests()
+                .requestMatchers("/").hasAnyRole("ADMIN", "GLOBAL_MANAGER", "CAFE_MANAGER")
                 .requestMatchers("/auth/login", "/error") //white list
                 .permitAll()
                 .anyRequest()
@@ -40,15 +41,21 @@ public class SecurityConfig {
                 .and()
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
-                .defaultSuccessUrl("/index", true)
-                .failureUrl("/auth/login?error")
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/auth/login?error");
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository
+                .findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
