98fd2c12fe07e72520bce8dc05ed43f7ad80caef
romanthium <romanthium@gmail.com>
2023-05-19 16:18:45 +0200

    moved new user creation from AuthenticationController to UserController

--- src/main/java/com/example/projectpizza/controller/auth/AuthenticationController.java
+++ src/main/java/com/example/projectpizza/controller/auth/AuthenticationController.java
@@ -1,53 +1,14 @@
 package com.example.projectpizza.controller.auth;
 
-import com.example.projectpizza.model.User;
-import com.example.projectpizza.service.UserRoleService;
-import com.example.projectpizza.service.UserService;
-import com.example.projectpizza.utils.validator.auth.UserValidator;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-
-    private final UserService userService;
-    private final UserRoleService userRoleService;
-
-    private final PasswordEncoder passwordEncoder;
-
-    private final UserValidator userValidator;
-
-    @GetMapping("/registration")
-    public String registrationPage(@ModelAttribute("user") User user, Model model) {
-        model.addAttribute("roles", userRoleService.findAll());
-        return "auth/registration";
-    }
-
-    @PostMapping("/registration")
-    public String registration(@ModelAttribute("user") @Valid User user,
-                               BindingResult bindingResult,
-                               Model model) {
-
-        userValidator.validate(user, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("roles", userRoleService.findAll());
-            return "auth/registration";
-        }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userService.save(user);
-        return "redirect:/";
-    }
 
     @GetMapping("/login")
     public String loginPage() {
