4526422b141c10af4af1903d0496f3aef245a283
romanthium <romanthium@gmail.com>
2023-05-24 15:08:00 +0200

    moved validation messages to file Ingredient model

--- src/main/java/com/example/projectpizza/model/Ingredient.java
+++ src/main/java/com/example/projectpizza/model/Ingredient.java
@@ -19,7 +19,7 @@ public class Ingredient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotBlank(message = "Name can'''t be empty")
+    @NotBlank(message = "{name.required}")
     private String name;
 
     @ManyToMany(mappedBy = "ingredients")
