f2e8d5079789d18cfeb800e0373726805f92cd92
romanthium <romanthium@gmail.com>
2023-06-04 17:51:32 +0200

    adde service annotation to EntityService

--- src/main/java/com/example/projectpizza/service/EntityService.java
+++ src/main/java/com/example/projectpizza/service/EntityService.java
@@ -1,8 +1,11 @@
 package com.example.projectpizza.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public interface EntityService<T> {
 
     List<T> findAll();
